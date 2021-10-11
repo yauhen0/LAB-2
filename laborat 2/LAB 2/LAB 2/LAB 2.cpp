4691ff9187923fed206139d66a9de25b2d6ffaa5
Yauhen Kondartiev <90138713+yauhen0@users.noreply.github.com>
2021-10-11 22:56:06 +0300

    Update LAB 2.cpp

--- laborat 2/LAB 2/LAB 2/LAB 2.cpp
+++ laborat 2/LAB 2/LAB 2/LAB 2.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void enterArr(int* arr, int size)               
 {
-    srand(time(NULL));
-    int size;
-    int selection;
-
-    cout << "Enter size of arr: ";
-    cin >> size;
-
-    int* arr = new int[size];
-
-    cout << "1.Enter from the keyboard" << endl;
-    cout << "2.Random values" << endl;
-
-    cin >> selection;
-
-
-    if (selection == 1)
+    cout << "Enter values" << endl;
+    for (int i = 0; i < size; i++)
     {
-        cout << "Enter values" << endl;
-        for (int i = 0; i < size; i++)
-        {
-            cin >> arr[i];
-        }
+        cin >> arr[i];
     }
- 
-    else if (selection == 2)
+}
+
+void randomArr(int* arr, int size)             
+{
+    int BeginRange;
+    int EndRange;
+
+    cout << "Enter range of numbers" << endl;
+    cout << "from: ";
+    cin >> BeginRange;
+    cout << "to: ";
+    cin >> EndRange;
+
+    for (int i = 0; i < size; i++)
     {
-        int BeginRange;
-        int EndRange;
-
-        cout << "Enter range of numbers" << endl;
-        cout << "from: ";
-        cin >> BeginRange;
-        cout << "to: ";
-        cin >> EndRange;
-
-        for (int i = 0; i < size; i++)
-        {
-            arr[i] = rand() % EndRange + BeginRange;
-        }
+        arr[i] = rand() % EndRange + BeginRange;
     }
-    
+}
+
+void swapArr(int* arr, int size)                 
+{
     int temp;
     for (int i = 0; i < size; i++)
     {
@@ -57,15 +42,20 @@ int main()
             }
         }
     }
-   
+}
+
+void printArr(int* arr, int size)               
+{
     cout << "Array: ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
+}
 
-   
+void sumArr(int* arr, int size)                 
+{
     int sum = 0;
     for (int i = 0; i < size; i++)
     {
@@ -74,9 +64,12 @@ int main()
             sum = sum + arr[i];
         }
     }
-    cout << "sum is: " << sum << endl;;
+    cout << "sum is: " << sum << endl;
+}
 
-   
+void mult(int* arr, int size)               
+{
+    
     int max_count = 0;
     int min_count = 0;
     for (int i = size - 1, j = 0; i > 0 && j < size; i--, j++)
@@ -94,27 +87,62 @@ int main()
         }
     }
 
-    
+ 
+    int mult = 1;
     for (int i = size - max_count - 1; i != 0 + min_count; i--)
     {
         mult = mult * arr[i];
     }
-    cout <<" Multiply:" << mult << endl;
+    cout << "Multiply: " << mult << endl;
+}
+
+void SortArr(int* arr, int size)                                 
+    int temp;
     for (int i = 0; i < (size / 2); i++)
     {
         for (int j = 0; j < size - i - 2; j++)
         {
             if (j % 2 != 0 && fabs(arr[j]) < fabs(arr[j + 2]))
             {
-                temp + arr[j];
+                temp = arr[j];
                 arr[j] = arr[j + 2];
                 arr[j + 2] = temp;
             }
         }
     }
-    for (int i = 0; i < size; i++)
+}
+
+
+int main()
+{
+    srand(time(NULL));
+    int size;
+    int selection;
+
+    cout << "Enter size of arr: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "1.Enter from the keyboard" << endl;
+    cout << "2.Random values" << endl;
+
+    cin >> selection;
+
+    if (selection == 1)
     {
-        cout << arr[i] << "";
+        enterArr(arr, size);
     }
-    cout << endl;
+    
+    else if (selection == 2)
+    {
+        randomArr(arr, size);
+    }
+   
+    swapArr(arr, size);
+    printArr(arr, size);
+    sumArr(arr, size);
+    mult(arr, size);
+    SortArr(arr, size);
+    printArr(arr, size);
 }
